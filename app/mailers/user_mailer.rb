5d4ff93273322d933e0cf0a3e0138aceafd9a314
David <djw2yw@virginia.edu>
2015-01-23 13:22:24 -0500

    Add password resets @ email config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,9 @@ class UserMailer < ActionMailer::Base
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
+  
 end
